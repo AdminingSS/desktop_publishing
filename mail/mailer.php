f7f995e3f74c88518eaf0c79a729009e62069b02
AdminingSS <admining@mail.ru>
2018-08-15 16:38:01 +0300

    file send added

--- mail/mailer.php
+++ mail/mailer.php
@@ -11,36 +11,53 @@
 * 4. Имя, с которого отправляется письмо (40 строчка)
 * 5. URL, на который будет переадресация, при успешной отправке формы (45 строчка)
 */
-    $subject = 'Почта пришла!!!';                      // тема письма , вместо многоточия вставьте ваш домен
-    $content = '';
+$subject = 'Почта пришла!!!';                      // тема письма , вместо многоточия вставьте ваш домен
+$content = '';
 
 //    if(isset($_POST['name'])) {
 //        $name = substr(htmlspecialchars(trim($_POST['name'])), 0, 100);
 //        $content .= '<b>Имя: </b>' . $name . '<br>';
 //    }
-    if(isset($_POST['email'])) {
-        $mail = substr(htmlspecialchars(trim($_POST['email'])), 0, 100);
-        $content .= '<b>Почта: </b>' . $mail . '<br>';
-    }
-    if(isset($_POST['message'])) {
-        $message = substr(htmlspecialchars(trim($_POST['message'])), 0, 100);
-        $content .= '<b>Сообщение: </b>' . $message . '<br>';
-    }
+if (isset($_POST['email'])) {
+    $mail = substr(htmlspecialchars(trim($_POST['email'])), 0, 100);
+    $content .= '<b>Почта: </b>' . $mail . '<br>';
+}
+if (isset($_POST['message'])) {
+    $message = substr(htmlspecialchars(trim($_POST['message'])), 0, 100);
+    $content .= '<b>Сообщение: </b>' . $message . '<br>';
+}
 
-    // подключаем файл класса для отправки почты
-    require 'class.phpmailer.php';
+$file = "";
 
-    $mail = new PHPMailer();
-    $mail->AddAddress('');      	// кому - адрес (например, 'email@ rek9.ru')
-    $mail->IsHTML(true);                        				// формат письма HTML
-    $mail->CharSet = "UTF-8";                   				// кодировка
-	$mail->From = "info@desktop-publishing.ru";					        	// email, с которого отправиться письмо
-	$mail->FromName = "DP";					    // от кого письмо
-    $mail->Body = $content;
-    $mail->Subject = $subject;
+// Если поле выбора вложения не пустое - закачиваем его на сервер
 
-    // отправляем наше письмо		
-	
-	if ($mail->Send()) header('Location: ../');                 // в поле Location можно настроить переадресацию
-	else { die ('Mailer Error: ' . $mail->ErrorInfo); }
+if (!empty($_FILES['mail_file']['tmp_name'])) {
+
+    // Закачиваем файл
+
+    $path = $_FILES['mail_file']['name'];
+
+    if (copy($_FILES['mail_file']['tmp_name'], $path)) $file = $path;
+}
+
+
+// подключаем файл класса для отправки почты
+require 'class.phpmailer.php';
+
+$mail = new PHPMailer();
+$mail->AddAddress('');        // кому - адрес (например, 'email@ rek9.ru')
+$mail->AddAttachment($file);
+$mail->IsHTML(true);                                        // формат письма HTML
+$mail->CharSet = "UTF-8";                                // кодировка
+$mail->From = "info@desktop-publishing.ru";                                // email, с которого отправиться письмо
+$mail->FromName = "DP";                        // от кого письмо
+$mail->Body = $content;
+$mail->Subject = $subject;
+
+// отправляем наше письмо
+
+if ($mail->Send()) header('Location: ../');                 // в поле Location можно настроить переадресацию
+else {
+    die ('Mailer Error: ' . $mail->ErrorInfo);
+}
 ?>
