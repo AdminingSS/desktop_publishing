faca7afb2df43cafef28f71e773c3d7ebace89fc
AdminingSS <admining@mail.ru>
2018-08-17 10:18:17 +0300

    many

--- js/app.js
+++ js/app.js
@@ -1,145 +1,201 @@
 $(document).ready(function () {
-
-    //Validator
     (function () {
-        $.validator.setDefaults({
-            submitHandler: function (form) {
 
-                const formData = $(form).serializeArray();
-                formData.push({
-                    name: 'file',
-                    value: files,
-                });
+        const files = [];
 
-                $.ajax({
-                    type: form.method,
-                    url: form.action,
-                    data: $.param(formData),
-                    success: function (response) {
-                        //console.log(response);
+        //Validator
+        (function () {
+            $.validator.setDefaults({
+                submitHandler: function (form) {
 
-                        if (response) {
-                            self.hidePending(form, self.showSuccess.bind(self, form));
+                    const formData = $(form).serializeArray();
+                    formData.push({
+                        name: 'file',
+                        value: files
+                    });
 
-                            if (resolve) {
-                                resolve.call(self, form, response);
-                            }
-                        } else {
-                            self.hidePending(form, self.showError.bind(self, form));
+                    $.ajax({
+                        type: form.method,
+                        url: form.action,
+                        data: $.param(formData),
+                        success: function (response) {
+                            // //console.log(response);
+                            //
+                            // if (response) {
+                            //     self.hidePending(form, self.showSuccess.bind(self, form));
+                            //
+                            //     if (resolve) {
+                            //         resolve.call(self, form, response);
+                            //     }
+                            // } else {
+                            //     self.hidePending(form, self.showError.bind(self, form));
+                            //
+                            //     if (reject) {
+                            //         reject.call(self, form, response);
+                            //     }
+                            // }
+                            //
+                            // self.resetForms(form);
+                        },
+                        error: function (response) {
 
-                            if (reject) {
-                                reject.call(self, form, response);
-                            }
+                            // //console.log(response);
+                            // //throw new Error(response.statusText);
+                            // self.hidePending(form, self.showError.bind(self, form));
+                            // self.resetForms(form);
+
                         }
+                    });
 
-                        self.resetForms(form);
+
+                    //$(form).ajaxSubmit();
+                }
+            });
+
+            const $sendForm = $('#sendForm');
+
+            const options = {
+                rules: {
+                    file: "required",
+                    email: {
+                        required: true,
+                        email: true
                     },
-                    error: function (response) {
-
-                        //console.log(response);
-                        //throw new Error(response.statusText);
-                        self.hidePending(form, self.showError.bind(self, form));
-                        self.resetForms(form);
-
+                    message: {
+                        required: false
                     }
-                });
-
-
-
-                //$(form).ajaxSubmit();
-            }
-        });
-
-        const $sendForm = $('#sendForm');
-
-        const options = {
-            rules: {
-                file: "required",
-                email: {
-                    required: true,
-                    email: true
                 },
-                message: {
-                    required: false
-                }
-            },
-            messages: {
-                file: "Пожалуйста, загрузите файл(ы)",
-                email: {
-                    required: "Пожалуйста, введите свой почтовый адрес",
-                    email: "Пожалуйста, введите корректный почтовый адрес"
-                }
-            },
-            errorElement: "em",
-            errorPlacement: function (error, element) {
-                // Add the `help-block` class to the error element
-                error.addClass("help-block");
+                messages: {
+                    file: "Пожалуйста, загрузите файл(ы)",
+                    email: {
+                        required: "Пожалуйста, введите свой почтовый адрес",
+                        email: "Пожалуйста, введите корректный почтовый адрес"
+                    }
+                },
+                errorElement: "em",
+                errorPlacement: function (error, element) {
+                    // Add the `help-block` class to the error element
+                    error.addClass("help-block");
 
-                if (element.prop("type") === "checkbox") {
-                    error.insertAfter(element.parent("label"));
-                } else {
-                    error.insertAfter(element);
+                    if (element.prop("type") === "checkbox") {
+                        error.insertAfter(element.parent("label"));
+                    } else {
+                        error.insertAfter(element);
+                    }
+                },
+                highlight: function (element, errorClass, validClass) {
+                    $(element).parents(".tm-highlight").addClass("has-error").removeClass("has-success");
+                },
+                unhighlight: function (element, errorClass, validClass) {
+                    $(element).parents(".tm-highlight").addClass("has-success").removeClass("has-error");
                 }
-            },
-            highlight: function (element, errorClass, validClass) {
-                $(element).parents(".tm-highlight").addClass("has-error").removeClass("has-success");
-            },
-            unhighlight: function (element, errorClass, validClass) {
-                $(element).parents(".tm-highlight").addClass("has-success").removeClass("has-error");
-            }
-        };
+            };
 
-        $sendForm.validate(options);
+            $sendForm.validate(options);
+        })();
+
+        //dz
+        (function () {
+
+            const dropzone = new Dropzone('#fileUpload', {
+                url: './mail/mauler.php',
+                //
+                previewTemplate: document.querySelector('#preview-template').innerHTML,
+                //parallelUploads: 2,
+                thumbnailHeight: 120,
+                thumbnailWidth: 120,
+                //maxFilesize: 3,
+                //filesizeBase: 1000,
+                thumbnail: function (file, dataUrl) {
+                    if (file.previewElement) {
+                        file.previewElement.classList.remove("dz-file-preview");
+                        var images = file.previewElement.querySelectorAll("[data-dz-thumbnail]");
+                        for (var i = 0; i < images.length; i++) {
+                            var thumbnailElement = images[i];
+                            thumbnailElement.alt = file.name;
+                            thumbnailElement.src = dataUrl;
+                        }
+                        setTimeout(function () {
+                            file.previewElement.classList.add("dz-image-preview");
+                        }, 1);
+                    }
+                    //
+                }
+            });
+            dropzone.on('addedfile', function (file) {
+                files.push(file);
+            });
+
+            // const $fileUpload = $("#fileUpload");
+            //
+            // // $fileUpload.dropzone({
+            // //     url: "./mail/mailer.php",
+            // //     parallelUploads: 5,
+            // //     uploadMultiple: true,
+            // //     autoProcessQueue: false,
+            // //     addRemoveLinks: true,
+            // //     previewsContainer: ".dropzone-previews",
+            // //     forceFallback: false
+            // //
+            // //     // init: function() {
+            // //     //     dzClosure = this; // Makes sure that 'this' is understood inside the functions below.
+            // //     //
+            // //     //     // for Dropzone to process the queue (instead of default form behavior):
+            // //     //     $(".filesend-submit").click(function (e) {
+            // //     //
+            // //     //         // Make sure that the form isn't actually being sent.
+            // //     //         e.preventDefault();
+            // //     //         e.stopPropagation();
+            // //     //         dzClosure.processQueue();
+            // //     //     });
+            // //     //
+            // //     //     //send all the form data along with the files:
+            // //     //     this.on("sendingmultiple", function(data, xhr, formData) {
+            // //     //         formData.append("email", $(".filesend-email").val());
+            // //     //         formData.append("message", $(".filesend-message").val());
+            // //     //     });
+            // //     // }
+            // //
+            // // });
+            //
+            // $fileUpload.on('addedfile',function (file) {
+            //     console.dir(arguments);
+            // });
+
+        })();
+
     })();
+
 
     //select2
     (function () {
         $('.tm-select').select2({
-            placeholder: 'Выберите валюту для оплаты',
+            placeholder: 'Выберите валюту',
             minimumResultsForSearch: Infinity
         });
     })();
 
-    //dz
+    //popper tooltip
     (function () {
 
-        $("#fileUpload").dropzone({
-            url: "../mail.mailer.php",
-            parallelUploads: 5,
-            uploadMultiple: true,
-            autoProcessQueue: false,
-            addRemoveLinks: true,
-            previewsContainer: ".dropzone-previews",
-            forceFallback: true,
-
-            init: function() {
-                dzClosure = this; // Makes sure that 'this' is understood inside the functions below.
-
-                // for Dropzone to process the queue (instead of default form behavior):
-                $(".filesend-submit").click(function (e) {
-
-                    // Make sure that the form isn't actually being sent.
-                    e.preventDefault();
-                    e.stopPropagation();
-                    dzClosure.processQueue();
-                });
-
-                //send all the form data along with the files:
-                this.on("sendingmultiple", function(data, xhr, formData) {
-                    formData.append("email", $(".filesend-email").val());
-                    formData.append("message", $(".filesend-message").val());
-                });
-            }
-
+        const $reference = $('.tm-tooltip');
+        $reference.each(function () {
+            const ref = this;
+            const anotherPopper = new Tooltip(ref, {
+                title: '<div>\n' +
+                '        <h2>\n' +
+                '            Заголовок\n' +
+                '        </h2>\n' +
+                '        <p>баран жираф и так далее, consectetur adipisicing elit. Asperiores dicta hic ipsam minima mollitia non placeat quaerat similique to</p>\n' +
+                '    </div>',
+                html: true
+            });
         });
 
     })();
 
-    //alternative file send
-    (function(){
-
-
-
-    })();
-
 });
+
+//function(){
+//                     return $($(ref).attr('title'))[0];
+//                 }
--- mail/mailer.php
+++ mail/mailer.php
@@ -1,23 +1,9 @@
 <?php
 
-/*
-* Денис Герасимов http://rek9.ru/
-* Данный скрипт обрабатывает форм и отправляет ее на email
-* В письме вы увидите utm метки, если использовали их в рекламной кампании
-* Измените в данном скрипте:
-* 1. Тему письма (13 строчку)
-* 2. Введите ваш email, на который отправлять обработанную форму (36 строчка)
-* 3. Email, с которого отправлять письмо (39 строчка)
-* 4. Имя, с которого отправляется письмо (40 строчка)
-* 5. URL, на который будет переадресация, при успешной отправке формы (45 строчка)
-*/
-$subject = 'Почта пришла!!!';                      // тема письма , вместо многоточия вставьте ваш домен
+$msg ='';
 $content = '';
+$subject = 'Почта пришла!!!';                      // тема письма , вместо многоточия вставьте ваш домен
 
-//    if(isset($_POST['name'])) {
-//        $name = substr(htmlspecialchars(trim($_POST['name'])), 0, 100);
-//        $content .= '<b>Имя: </b>' . $name . '<br>';
-//    }
 if (isset($_POST['email'])) {
     $mail = substr(htmlspecialchars(trim($_POST['email'])), 0, 100);
     $content .= '<b>Почта: </b>' . $mail . '<br>';
@@ -27,32 +13,43 @@ if (isset($_POST['message'])) {
     $content .= '<b>Сообщение: </b>' . $message . '<br>';
 }
 
-$file = "";
-
-// Если поле выбора вложения не пустое - закачиваем его на сервер
-
-if (!empty($_FILES['mail_file']['tmp_name'])) {
-
-    // Закачиваем файл
-
-    $path = $_FILES['mail_file']['name'];
-
-    if (copy($_FILES['mail_file']['tmp_name'], $path)) $file = $path;
-}
-
-
 // подключаем файл класса для отправки почты
-require 'class.phpmailer.php';
 
+require 'class.phpmailer.php';
 $mail = new PHPMailer();
-$mail->AddAddress('');        // кому - адрес (например, 'email@ rek9.ru')
-$mail->AddAttachment($file);
-$mail->IsHTML(true);                                        // формат письма HTML
+$mail->AddAddress('admining@mail.ru');        // кому - адрес, Имя (например, 'email@ rek9.ru','Денис Герасимов')
+$mail->IsHTML(true);                                        // выставляем формат письма HTML
 $mail->CharSet = "UTF-8";                                // кодировка
-$mail->From = "info@desktop-publishing.ru";                                // email, с которого отправиться письмо
-$mail->FromName = "DP";                        // от кого письмо
-$mail->Body = $content;
+$mail->From = "info@chistu.ru";                                // email, с которого отправиться письмо
+$mail->FromName = "Chistu";                        // откого письмо
+
 $mail->Subject = $subject;
+
+if (array_key_exists('file', $_FILES)) {
+
+    for ($ct = 0; $ct < count($_FILES['file']['tmp_name']); $ct++) {
+
+        $uploadfile = tempnam(sys_get_temp_dir(), hash('sha256', $_FILES['file']['name'][$ct]));
+
+        $filename = $_FILES['file']['name'][$ct];
+
+        if (move_uploaded_file($_FILES['file']['tmp_name'][$ct], $uploadfile)) {
+
+            $mail->AddAttachment($uploadfile, $filename);
+
+        } else {
+
+            $msg .= 'Failed to move file to ' . $uploadfile;
+
+        }
+
+    }
+
+}
+$content .= '<b>Сообщение ошибки: </b>' . $msg . '<br>';
+$content .= '<b>Имя файла: </b>' . $filename . '<br>';
+
+$mail->Body = $content;
 
 // отправляем наше письмо
 
